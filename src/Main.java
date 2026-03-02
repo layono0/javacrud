2cf86faacb50e3266eedddccbb4c47f6c085b6bc
dev <fdenub@gmail.com>
2026-03-02 11:15:13 -0300

    leves alterações

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,18 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
+    static void menuMostrar(){
+        String menuText = """
+                Menu insano do caba
+                1- listar alunos
+                2- criar alunos
+                3- excluir alunos
+                4- atualizar alunos
+                """;
+        System.out.println(menuText);
+    }
+
     static void main(){
         List<Alunos> listaAlunos = new ArrayList<>();
 
@@ -10,23 +22,47 @@ public class Main {
         listaAlunos.add(novoaluno);
 
 
+        menuMostrar();
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("digite o nome do aluno: ");
-        String nome = scanner.nextLine();
+//        System.out.print("digite o nome do aluno: ");
+//        String nome = scanner.nextLine();
+//
+//        System.out.print("digite a idade do aluno: ");
+//        String idadeString = scanner.nextLine();
+//        int idade = Integer.parseInt(idadeString);
+//
+//        System.out.print("digite a matricula do aluno: ");
+//        String matricula = scanner.nextLine();
+//
+//        Alunos alunoscanner = new Alunos(nome, idade, matricula);
+//
+//        listaAlunos.add(alunoscanner);
+//        System.out.println(listaAlunos);
+        System.out.print("Escolha a opção desejada: " + listaAlunos.get(0).getMatricula());
+        var opcaoTexto = scanner.nextLine();
 
-        System.out.print("digite a idade do aluno: ");
-        String idadeString = scanner.nextLine();
-        int idade = Integer.parseInt(idadeString);
+        int opcao = Integer.parseInt(opcaoTexto);
 
-        System.out.print("digite a matricula do aluno: ");
-        String matricula = scanner.nextLine();
+        switch(opcao){
+            case 1:
+                System.out.println("opcao 1 selecionada");
+                System.out.println(listaAlunos);
+                break;
+            case 2:
+                System.out.println("opcao 2 selecionada");
+                break;
+            case 3:
+                System.out.println("opcao 3 selecionada");
+                break;
+            case 4:
+                System.out.println("opcao 4 selecionada");
+                break;
 
-        Alunos alunoscanner = new Alunos(nome, idade, matricula);
+        }
 
-        listaAlunos.add(alunoscanner);
-        System.out.println(listaAlunos);
+
 
     }
 }
