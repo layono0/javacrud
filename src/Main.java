d10dce99071b8a773e190fa55e5ac56d0a38163e
layono0 <botdonegativih@gmail.com>
2026-03-05 06:45:16 -0300

    excluir completo

--- src/Main.java
+++ src/Main.java
@@ -5,98 +5,133 @@ import java.util.regex.Pattern;
 
 public class Main {
     private static ArrayList<Alunos> listaAlunos = new ArrayList<>();
-        public static void main(String[] args){
-            menuAlunos();
 
-        }
+    public static void main(String[] args) {
+        menuAlunos();
 
-        public static void menuAlunos(){
-            String menuText = """
+    }
+
+    public static void menuAlunos() {
+        String menuText = """
                 Menu insano do caba
                 1- listar alunos
                 2- criar alunos
                 3- excluir alunos
                 4- atualizar alunos
                 """;
-            System.out.println(menuText);
-            String opcao = Ler.dados("Selecione sua opção: ");
-            switch (opcao){
-                case "1":
-                    System.out.println(listaAlunos);
-                    break;
-                case "2":
-                    criarAluno();
-                    break;
-                case "3":
-                    excluirObj();
-                    break;
-                case "4":
-                    System.out.println("opcao 4");
-                    break;
-                default:
-                    System.out.println("Opção Inválida, tente novamente!");
-                    menuAlunos();
-            }
+        System.out.println(menuText);
+        String opcao = Ler.dados("Selecione sua opção: ");
+        switch (opcao) {
+            case "1":
+                System.out.println(listaAlunos);
+                break;
+            case "2":
+                criarAluno();
+                break;
+            case "3":
+                excluirObj();
+                break;
+            case "4":
+                System.out.println("opcao 4");
+                break;
+            default:
+                System.out.println("Opção Inválida, tente novamente!");
+                menuAlunos();
         }
-        private static void criarAluno(){
-            int idadeNum = 0;
-            String nome = Ler.dados("Nome do aluno: ").replaceAll("\\\\s", "");;
-            while(!isLetra(nome)){
-                System.out.print("o nome deve ser digitado com letras e sem espacos");
-                nome = Ler.dados("Nome do aluno: ");
-            }
-            String idade = Ler.dados("Idade do aluno");
-            while(!isNumero(idade)){
-                System.out.print("a idade deve ser um número: ");
-                idade = Ler.dados("Idade do aluno: ");
+    }
 
+    private static void criarAluno() {
+        int idadeNum = 0;
+        String nome = Ler.dados("Nome do aluno: ").replaceAll("\\\\s", "");
+        ;
+        while (!isLetra(nome)) {
+            System.out.print("o nome deve ser digitado com letras e sem espacos");
+            nome = Ler.dados("Nome do aluno: ");
+        }
+        String idade = Ler.dados("Idade do aluno");
+        while (!isNumero(idade)) {
+            System.out.print("a idade deve ser um número: ");
+            idade = Ler.dados("Idade do aluno: ");
 
-            }
-            idadeNum = Integer.parseInt(idade);
-            String matricula = Ler.dados("Matricula do aluno: ").replaceAll("\\\\s", "");
-
-            Alunos alunoNovo = new Alunos(nome, idadeNum, matricula);
-            listaAlunos.add(alunoNovo);
-            System.out.println(listaAlunos);
-            menuAlunos();
 
         }
+        idadeNum = Integer.parseInt(idade);
+        String matricula = Ler.dados("Matricula do aluno: ").replaceAll("\\\\s", "");
 
-        private static boolean isNumero(String numero){
+        Alunos alunoNovo = new Alunos(nome, idadeNum, matricula);
+        listaAlunos.add(alunoNovo);
+        System.out.println(listaAlunos);
+        menuAlunos();
+
+    }
+
+    private static boolean isNumero(String numero) {
 //
 //            if(numero == null && numero.isBlank()){
 //                return  false;
 //            }
 
-            try {
-                Integer.parseInt(numero);
-                return true;
-            }
-            catch (NumberFormatException e){
-                return  false;
-            }
-
-
+        try {
+            Integer.parseInt(numero);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
         }
-        private static boolean isLetra(String str){
-            if(!Pattern.matches("[a-zA-Z]+", str)){
-                return false;
-            }
-            else{
-                return true;
-            }
-
-
-        }
-        private static void excluirObj(){
-            for(Object obj : listaAlunos){
-                System.out.println(obj);
-            }
-        }
-
-
-
 
 
     }
 
+    private static boolean isLetra(String str) {
+        if (!Pattern.matches("[a-zA-Z]+", str)) {
+            return false;
+        } else {
+            return true;
+        }
+
+
+    }
+    private static boolean opcaoisValida(String str){
+        if(str.equals("S") && str.equals("N")){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    private static int diminuiUm(int indice){
+        return indice + 1;
+    }
+
+    private static void excluirObj() {
+        int contagem = 0;
+        System.out.println("Alunos cadastrados no momento");
+        for (Alunos aluno : listaAlunos) {
+            contagem++;
+            System.out.println(contagem + " - " + aluno.getNome());
+
+        }
+        String opcaoStr = Ler.dados("Digite o numero referente ao aluno que deseja excluir: ");
+        while (!isNumero(opcaoStr)) {
+            System.out.print("A opcao desejada DEVE ser um numero.");
+            opcaoStr = Ler.dados("Numero referente ao aluno: ");
+
+        }
+        int opcao = diminuiUm(Integer.parseInt(opcaoStr));
+        String confirm = Ler.dados("Voce deseja MESMO excluir o aluno? confirme com S/N: ");
+        while(!opcaoisValida(confirm)){
+            System.out.println("Opcao invalida! a escolha deve ser S ou N");
+            confirm = Ler.dados("Voce deseja MESMO excluir o aluno? confirme com S/N: ");
+        }
+        if(confirm.equals("S")){
+            listaAlunos.remove(opcao);
+        }
+        else{
+            System.out.println("O usuario nao foi removido...");
+        }
+
+
+        System.out.println(listaAlunos);
+
+    }
+
+}
